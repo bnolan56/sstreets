d1b0ae173b24ec16efdb95bee949e77ac9671808
Brad Nolan <bnolan56@gmail.com>
2019-01-15 09:29:47 -0700

    idk wtf to do

--- example.js
+++ example.js
@@ -1,39 +1,5 @@
-// import * as SmartyStreetsSDK from "smartystreets-javascript-sdk";
-// const SmartyStreetsSDK = require('smartystreets-javascript-sdk');
-// const SmartyStreetsSDK = require("smartystreets-javascript-sdk");
+import * as SmartyStreetsSDK from "smartystreets-javascript-sdk";
 
-// const SmartyStreetsCore = SmartyStreetsSDK.core;
-// const Lookup = SmartyStreetsSDK.usStreet.Lookup;
-//
-// // Add your credentials to a credentials object.
-// let authId = "aa44dd3c-fa98-919a-40b9-a083a34b8840";
-// let authToken = "1S77gUHbZv0KeSw8fX89";
-// let credentials = new SmartyStreetsCore.StaticCredentials(authId, authToken);
-//
-// // Build a client.
-// let clientBuilder = new SmartyStreetsCore.ClientBuilder(credentials);
-// let client = clientBuilder.buildUsStreetApiClient();
-//
-// // Create and populate a lookup.
-// let lookup1 = new Lookup();
-// lookup1.street = "1600 Pennsylvania Ave NW";
-// lookup1.zipCode = "20500";
-//
-// // Send the lookup from the client and handle the response.
-// client.send(lookup1)
-// 	.then(handleSuccess)
-// 	.catch(handleError);
-//
-// function handleSuccess(response) {
-// 	// Log the lookup results to the console.
-// 	response.lookups.map(lookup => console.log(lookup.result));
-// }
-//
-// function handleError(response) {
-// 	console.log(response);
-// }
-
-// // Add your credentials to a credentials object.
 const SmartyStreetsCore = SmartyStreetsSDK.core;
 const Lookup = SmartyStreetsSDK.usAutocomplete.Lookup;
 
@@ -43,7 +9,7 @@ let credentials = new SmartyStreetsCore.SharedCredentials(websiteKey)
 
 let clientBuilder = new SmartyStreetsCore.ClientBuilder(credentials);
 let client = clientBuilder.buildUsAutocompleteClient();
-let lookup = new Lookup("6471 South Cody Way");
+let lookup = new Lookup("1080 Pasito");
 
 client.send(lookup)
 	.then(logSuggestions)
@@ -51,18 +17,110 @@ client.send(lookup)
 
 function logSuggestions(response) {
 	console.log(response.result);
-	$(".address1").html("<select>" + "<option>" + response.result + "</option></select>");
+
+	function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+autocomplete(document.getElementsByClassName("address1"), addressList);
 }
 
 function handleError(response) {
 	console.log(response);
-
 }
 
-// Event Listener to push address into lookup
-$(function() {
-    $(".address1").on("input", function() {
-				console.log("key pressed")
-				lookup = $(".address1").val().trim();
-		});
-});
+var addressList= response.result;
