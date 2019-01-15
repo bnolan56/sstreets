e84212b020bf5a972d335edc33829f60f2e1ae81
Brad Nolan <bnolan56@gmail.com>
2019-01-15 13:04:59 -0700

    fixing up smarty streets

--- example.js
+++ example.js
@@ -1,79 +1,54 @@
-// import * as SmartyStreetsSDK from "smartystreets-javascript-sdk";
-// const SmartyStreetsSDK = require('smartystreets-javascript-sdk');
-// const SmartyStreetsSDK = require("smartystreets-javascript-sdk");
-
-// const SmartyStreetsCore = SmartyStreetsSDK.core;
-// const Lookup = SmartyStreetsSDK.usStreet.Lookup;
-//
-// // Add your credentials to a credentials object.
-// let authId = "aa44dd3c-fa98-919a-40b9-a083a34b8840";
-// let authToken = "1S77gUHbZv0KeSw8fX89";
-// let credentials = new SmartyStreetsCore.StaticCredentials(authId, authToken);
-//
-// // Build a client.
-// let clientBuilder = new SmartyStreetsCore.ClientBuilder(credentials);
-// let client = clientBuilder.buildUsStreetApiClient();
-//
-// // Create and populate a lookup.
-// let lookup1 = new Lookup();
-// lookup1.street = "1600 Pennsylvania Ave NW";
-// lookup1.zipCode = "20500";
-//
-// // Send the lookup from the client and handle the response.
-// client.send(lookup1)
-// 	.then(handleSuccess)
-// 	.catch(handleError);
-//
-// function handleSuccess(response) {
-// 	// Log the lookup results to the console.
-// 	response.lookups.map(lookup => console.log(lookup.result));
-// }
-//
-// function handleError(response) {
-// 	console.log(response);
-// }
-
-// // Add your credentials to a credentials object.
+/* Initialize the SDK. */
 const SmartyStreetsCore = SmartyStreetsSDK.core;
 const Lookup = SmartyStreetsSDK.usAutocomplete.Lookup;
+const websiteKey = "1623204754651110"; // AHS API key.
+const credentials = new SmartyStreetsCore.SharedCredentials(websiteKey)
+const clientBuilder = new SmartyStreetsCore.ClientBuilder(credentials);
+const client = clientBuilder.buildUsAutocompleteClient();
 
-// Add your credentials to a credentials object.
-let websiteKey = "1623204754651110";
-let credentials = new SmartyStreetsCore.SharedCredentials(websiteKey)
+/* Bind to DOM elements for future use */
+const addressInput = document.getElementById("smartystreets-autocomplete-input");
+const candidateOutput = document.getElementById("smartystreets-autocomplete-output");
 
-let clientBuilder = new SmartyStreetsCore.ClientBuilder(credentials);
-let client = clientBuilder.buildUsAutocompleteClient();
-let lookup = new Lookup("");
+/* Create an event listener on the input. */
+addressInput.addEventListener("input", e => {
+	// Get the input value.
+	const textInput = e.target.value;
 
-client.send(lookup)
-	.then(logSuggestions)
-	.catch(handleError);
+  // Create a new lookup with the input value.
+  const lookup = new Lookup(textInput);
 
-function logSuggestions(response) {
-	console.log(response.result);
-	lookupResult = response.result;
-	// $(".address1").html("<select>" + "<option>" + response.result + "</option></select>");
-	for (var i = 0; i < lookupREsult.length; i++) {
-		$( "<select><option>" ).text(lookupResult[i]);
-	}
-}
+  // Add any additional optional configuration to the lookup, eg. filtering, etc.
+  // Options for configuring the lookup can be found in the SDK source code:
+  // https://github.com/smartystreets/smartystreets-javascript-sdk/blob/master/src/us_autocomplete/Lookup.js
+  lookup.stateFilter = ["NY"];
+  // lookup.cityFilter = ["New York"];
 
-function handleError(response) {
-	console.log(response);
+  // Clear old output to make room for new results.
+	candidateOutput.innerHTML = null;
 
-}
-
-// Event Listener to push address into lookup
-$(function() {
-    // $(".address1").on("input", function() {
-		// 		console.log("key pressed")
-		// 		lookup = $(".address1").val().trim();
-		// });
-
-		$( ".address1" ).keyup(function() {
-	    var value = $( this ).val();
-			let lookup = new Lookup(value);
-
-	  })
-  	.keyup();
+	// Send the lookup with the SDK. The SDK returns a promise.
+  client.send(lookup)
+    .then(handleResponse)
+    .catch(handleError);
 });
+
+/*
+Do something with the SDK response data. In this case, we pass the data to
+another function to append it to the DOM.
+*/
+function handleResponse(response) {
+  response.result.map(buildTextOutput);
+}
+
+/* Handle any errors. */
+function handleError(response) {
+	console.warn(response);
+}
+
+function buildTextOutput(candidate) {
+	const outputElement = document.createElement("div");
+  outputElement.innerHTML = candidate.text;
+
+  candidateOutput.appendChild(outputElement);
+}
